a5c4dbccb414219cf8de7df3ff19f6513bf26c15
BogdanCozma14 <116218327+BogdanCozma14@users.noreply.github.com>
2023-03-25 16:14:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// const text = document.querySelector(".text .sec-text");
+//
+// const textLoad = () => {
+//   setTimeout(() => {
+//     text.textContent = "Programmer";
+//   }, 0);
+//
+//   setTimeout(() => {
+//     text.textContent = "College Student";
+//   }, 4000);
+//
+//   setTimeout(() => {
+//     text.textContent = "Freelancer";
+//   }, 8000);
+// }
+//   textLoad();
+// setInterval(textLoad, 12000;
